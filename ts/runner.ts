7644a1c0274105f869ccc944f1eef355289e967b
Wekoslav Stefanovski <swekster@gmail.com>
2022-09-16 16:30:43 +0200

    Solution to 1770 (dp)

--- ts/runner.ts
+++ ts/runner.ts
@@ -1,3 +1,4 @@
+import { performance } from 'perf_hooks';
 import { Problem } from './model';
 import { compare } from './utils';
 
@@ -8,8 +9,14 @@ async function main() {
         const problem = require(`./problems/${id}.ts`).problem as Problem;
 
         for (const test of problem.tests) {
+            const start = performance.now();
             const actual = problem.solution(test.argument);
+            const end = performance.now();
+            const duration = (end - start) | 0;
             const expected = test.result;
+            if (duration > 1) {
+                console.log(chalk.yellow(`${id}: Test case ${test.id} duration: ${duration}`));
+            }
             if (compare(actual, expected)) {
                 console.log(chalk.green(`${id}: passed test case ${test.id}`));
             } else {
