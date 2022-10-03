98133c1d4b844275fab25525ff1909241aa8d8e8
Wekoslav Stefanovski <swekster@gmail.com>
2022-10-03 09:34:46 +0200

    Solution to 1155

--- ts/problems/1155.ts
+++ ts/problems/1155.ts
@@ -1,38 +1,48 @@
 import { Problem } from "../model";
 import { TreeNode, wrapTree} from "../tree-utils";
+import { paramWrapper } from "../utils";
 
-export const problem: Problem<(number|null)[], string> = {
-    name: 'Construct String from Binary Tree',
-    id: 606,
-    url: 'https://leetcode.com/problems/construct-string-from-binary-tree/',
-    solution: wrapTree(tree2str),
+export const problem: Problem<[number, number, number], number> = {
+    name: 'Number of Dice Rolls With Target Sum',
+    id: 1155,
+    url: 'https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/',
+    solution: paramWrapper(numRollsToTarget),
     tests: [
         {
             id: 1,
-            argument: [1,2,3,4],
-            result: "1(2(4))(3)"
+            argument: [1, 6, 3],
+            result: 1
         },
         {
             id: 2,
-            argument: [1,2,3,null,4],
-            result: "1(2()(4))(3)"
-        }
+            argument: [2, 6, 7],
+            result: 6
+        },
+        {
+            id: 3,
+            argument: [30, 30, 500],
+            result: 222616187
+        },
     ]
 }
 
-function tree2str(root: TreeNode | null): string {
-    if (root === null) {
-        return "";
+const modulo = 1_000_000_007;
+
+const memo: Record<string, number> = {};
+
+function numRollsToTarget(n: number, k: number, target: number): number {
+    const key = `${n}:${k}:${target}`;
+    if (memo[key] !== undefined) {
+        return memo[key];
     }
-    let result = `${root.val}`;
-    if (root.right === null) {
-        if (root.left === null) {
-            return result;
-        }
-        return `${result}(${tree2str(root.left)})`;
+    if (n <= 0) {
+        return target === 0 ? 1 : 0;
     }
-    if (root.left === null) {
-        return `${result}()(${tree2str(root.right)})`;
+    let result = 0;
+    for (let i = 1; i <= k; i++) {
+        result += numRollsToTarget(n - 1, k, target - i);
+        result %= modulo;
     }
-    return `${result}(${tree2str(root.left)})(${tree2str(root.right)})`;
+    memo[key] = result;
+    return result;
 };
