529670783303167929d90eaabb561871bfc55b84
Wekoslav Stefanovski <swekster@gmail.com>
2024-03-05 10:06:45 +0100

    Solutions march, 2024 (1750)

--- ts/problems/1750.ts
+++ ts/problems/1750.ts
@@ -4,7 +4,7 @@ import { paramWrapper } from "../utils";
 export const problem: Problem<string, number> = {
     name: 'Minimum Length of String After Deleting Similar Ends',
     id: 1750,
-    url: 'https://leetcode.com/problems/maximum-units-on-a-truck/',
+    url: 'https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/',
     solution: minimumLength,
     tests: [
         {
@@ -21,10 +21,42 @@ export const problem: Problem<string, number> = {
             id: 3,
             argument: "aabccabba",
             result: 3
+        },
+        {
+            id: 4,
+            argument: "a",
+            result: 1
+        },
+        {
+            id: 5,
+            argument: "aabbccacba",
+            result: 1
+        },
+        {
+            id: 6,
+            argument: "aa",
+            result: 0
         }
     ]
 }
 
 function minimumLength(s: string): number {
-    return 0;
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            break;
+        }
+        while (s[left] === s[left + 1]) {
+            left += 1;
+        }
+        while (s[right] === s[right - 1]) {
+            right -= 1;
+        }
+        left += 1;
+        right -= 1;
+    }
+
+    return Math.max(right - left + 1, 0);
 };
